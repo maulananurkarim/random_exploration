5904b815661067b9602651681ad8b9ccf7a3e5fc
Maulana Nurkarim <maulananurkarim.official@gmail.com>
2021-05-01 19:20:30 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,129 +20,145 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
 
   @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int id = 5;
+
+  @override
+  void setState(id) {
+    super.setState(id);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: RefreshIndicator(
-        onRefresh: () async => Future.delayed(Duration(seconds: 2)),
-        child:
-            ListView(physics: const AlwaysScrollableScrollPhysics(), children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 30,
-              right: 30,
-            ),
-            child: Column(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: RefreshIndicator(
+          onRefresh: () async => Future.delayed(Duration(seconds: 2)),
+          child: ListView(
+              physics: const AlwaysScrollableScrollPhysics(),
               children: [
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  "Profile Picture",
-                  style: GoogleFonts.poppins(
-                    textStyle: titleTextStyle,
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 30,
+                    right: 30,
                   ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Image.asset(
-                  "assets/item-3.png",
-                  width: 140,
-                  height: 140,
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  "Maulana Nurkarim",
-                  style: GoogleFonts.poppins(
-                    textStyle: mediumtitleTextStyle,
-                  ),
-                ),
-                SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  "Product Designer",
-                  style: GoogleFonts.poppins(textStyle: subtitleTextStyle),
-                ),
-                SizedBox(
-                  height: 70,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset(
-                      "assets/item-1.png",
-                      height: 80,
-                      width: 80,
-                    ),
-                    Image.asset(
-                      "assets/item-2.png",
-                      height: 80,
-                      width: 80,
-                    ),
-                    Image.asset(
-                      "assets/item-3.png",
-                      height: 80,
-                      width: 80,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 40),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset(
-                      "assets/item-4.png",
-                      height: 80,
-                      width: 80,
-                    ),
-                    Image.asset(
-                      "assets/item-5.png",
-                      height: 80,
-                      width: 80,
-                    ),
-                    Image.asset(
-                      "assets/item-6.png",
-                      height: 80,
-                      width: 80,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 70,
-                ),
-                SizedBox(
-                  width: 224,
-                  height: 55,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      actionButton(context);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 50,
                       ),
-                    ),
-                    child: Text(
-                      "Update Profile",
-                      style:
-                          GoogleFonts.poppins(textStyle: mediumtitleTextStyle)
-                              .copyWith(fontSize: 16),
-                    ),
+                      Text(
+                        "Profile Picture",
+                        style: GoogleFonts.poppins(
+                          textStyle: titleTextStyle,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Image.asset(
+                        "assets/item-" + id.toString() + ".png",
+                        width: 140,
+                        height: 140,
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        "Maulana Nurkarim",
+                        style: GoogleFonts.poppins(
+                          textStyle: mediumtitleTextStyle,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        "Product Designer",
+                        style:
+                            GoogleFonts.poppins(textStyle: subtitleTextStyle),
+                      ),
+                      SizedBox(
+                        height: 70,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset(
+                            "assets/item-1.png",
+                            height: 80,
+                            width: 80,
+                          ),
+                          Image.asset(
+                            "assets/item-2.png",
+                            height: 80,
+                            width: 80,
+                          ),
+                          Image.asset(
+                            "assets/item-3.png",
+                            height: 80,
+                            width: 80,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 40),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset(
+                            "assets/item-4.png",
+                            height: 80,
+                            width: 80,
+                          ),
+                          Image.asset(
+                            "assets/item-5.png",
+                            height: 80,
+                            width: 80,
+                          ),
+                          Image.asset(
+                            "assets/item-6.png",
+                            height: 80,
+                            width: 80,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 70,
+                      ),
+                      SizedBox(
+                        width: 224,
+                        height: 55,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            actionButton(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                          ),
+                          child: Text(
+                            "Update Profile",
+                            style: GoogleFonts.poppins(
+                                    textStyle: mediumtitleTextStyle)
+                                .copyWith(fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
-          )
-        ]),
+                )
+              ]),
+        ),
       ),
     );
   }
