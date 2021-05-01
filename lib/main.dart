a9b1d58645f62815feb8203117886fd5d9c4d879
Maulana Nurkarim <maulananurkarim.official@gmail.com>
2021-05-01 16:27:39 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,45 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       body: Column(
         children: [
-          Text("data")
+          SizedBox(
+            height: 50,
+          ),
+          Text("Profile Picture"),
+          SizedBox(
+            height: 50,
+          ),
+          Image.asset("primary.png"),
+          SizedBox(
+            height: 16,
+          ),
+          Text("Anne Margaritha"),
+          SizedBox(
+            height: 4,
+          ),
+          Text("UX Designer"),
+          Row(
+            children: [
+              Image.asset("item-1.png"),
+              Image.asset("item-2.png"),
+              Image.asset("item-3.png"),
+            ],
+          ),
+          SizedBox(height: 40),
+          Row(
+            children: [
+              Image.asset("item-4.png"),
+              Image.asset("item-5.png"),
+              Image.asset("item-6.png"),
+            ],
+          ),
+          SizedBox(height: 70),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Update Profile"),
+          ),
         ],
       ),
     );
