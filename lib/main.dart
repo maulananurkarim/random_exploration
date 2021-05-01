855f2fbdc94730f65b41adab42d6087f493908ba
Maulana Nurkarim <maulananurkarim.official@gmail.com>
2021-05-01 17:20:10 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:random_exploration/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
     );
   }
@@ -20,45 +23,113 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          SizedBox(
-            height: 50,
-          ),
-          Text("Profile Picture"),
-          SizedBox(
-            height: 50,
-          ),
-          Image.asset("primary.png"),
-          SizedBox(
-            height: 16,
-          ),
-          Text("Anne Margaritha"),
-          SizedBox(
-            height: 4,
-          ),
-          Text("UX Designer"),
-          Row(
-            children: [
-              Image.asset("item-1.png"),
-              Image.asset("item-2.png"),
-              Image.asset("item-3.png"),
-            ],
-          ),
-          SizedBox(height: 40),
-          Row(
-            children: [
-              Image.asset("item-4.png"),
-              Image.asset("item-5.png"),
-              Image.asset("item-6.png"),
-            ],
-          ),
-          SizedBox(height: 70),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text("Update Profile"),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 30,
+          right: 30,
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Profile Picture",
+              style: GoogleFonts.poppins(
+                textStyle: titleTextStyle,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Image.asset(
+              "assets/primary.png",
+              width: 140,
+              height: 140,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              "Anne Margaritha",
+              style: GoogleFonts.poppins(
+                textStyle: mediumtitleTextStyle,
+              ),
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Text(
+              "UX Designer",
+              style: GoogleFonts.poppins(textStyle: subtitleTextStyle),
+            ),
+            SizedBox(
+              height: 70,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset(
+                  "assets/item-1.png",
+                  height: 80,
+                  width: 80,
+                ),
+                Image.asset(
+                  "assets/item-2.png",
+                  height: 80,
+                  width: 80,
+                ),
+                Image.asset(
+                  "assets/item-3.png",
+                  height: 80,
+                  width: 80,
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset(
+                  "assets/item-4.png",
+                  height: 80,
+                  width: 80,
+                ),
+                Image.asset(
+                  "assets/item-5.png",
+                  height: 80,
+                  width: 80,
+                ),
+                Image.asset(
+                  "assets/item.png",
+                  height: 80,
+                  width: 80,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 70,
+            ),
+            SizedBox(
+              width: 224,
+              height: 55,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: Text(
+                  "Update Profile",
+                  style: GoogleFonts.poppins(textStyle: mediumtitleTextStyle)
+                      .copyWith(fontSize: 16),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+Color greenColor = Color(0xff314728);
+Color greyColor = Color(0xffA9B0A6);
+
+TextStyle titleTextStyle = TextStyle(
+  color: greenColor,
+  fontWeight: FontWeight.w600,
+  fontSize: 20,
+);
+
+TextStyle subtitleTextStyle = TextStyle(
+  color: greyColor,
+  fontWeight: FontWeight.normal,
+  fontSize: 16,
+);
+
+TextStyle mediumtitleTextStyle = TextStyle(
+  color: greenColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 18,
+);
